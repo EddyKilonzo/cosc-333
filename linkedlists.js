c2c0aeda5c3bb4436690b7a471348bb717ade06d
Eddy Kilonzo <eddymax3715@gmail.com>
2025-04-14 12:41:36 +0300

    example of linked list

---
+++ linkedlists.js
@@ -0,0 +1,43 @@
+class LinkedListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  add(value) {
+    const newNode = new LinkedListNode(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  print() {
+    let current = this.head;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+}
+
+function main() {
+  const list = new LinkedList();
+  list.add(1);
+  list.add(2);
+  list.add(3);
+  list.print();
+}
+
+main();
